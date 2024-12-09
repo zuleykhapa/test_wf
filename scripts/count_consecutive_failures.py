4c1fcc04d0c8577aa6eb76ff78c69a5c4b5e4438
zuleykhapa <ya-zu@yandex.ru>
2024-12-09 16:44:30 +0100

    some changes

--- scripts/count_consecutive_failures.py
+++ scripts/count_consecutive_failures.py
@@ -26,14 +26,32 @@ def count_consecutive_failures():
             FROM '{ input_file }'
             )
         WHERE conclusion = 'success'
-        LIMIT 1
     """).fetchone()
-    failures = result[0] if result else 'None'
+    last_success = result[0] if result else -1
 
-    if failures > 0:
-        failures_list = "failures_list.md"
+    if last_success == 0:
+        with open("nightly_failures_{}.md".format(nightly_build), 'a') as f:
+            f.write(f"\n**{ nightly_build }** nightly-build has succeeded.\n")
+            url = duckdb.sql(f"SELECT url FROM '{ input_file }'").fetchone()[0]
+            f.write(f"Latest run: [ Run Link ]({ url })\n")
+    else:
+        if last_success == -1:
+            last_success = 50
         with open("nightly_failures_{}.md".format(nightly_build), 'w') as f:
-            f.write(f"\n**{ nightly_build }** nightly-build has not succeeded the previous **{ failures }** times.\n")
+            f.write(f"\n**{ nightly_build }** nightly-build has not succeeded the previous **{ last_success }** times.\n")
+            successful_run_url = duckdb.sql(f"""
+                SELECT
+                    url
+                FROM (
+                    SELECT
+                        row_number() over(),
+                        *
+                    FROM '{ input_file }'
+                )
+                WHERE conclusion = 'success'
+            """).fetchone()[0]
+            f.write(f"Last successfull run: [ Run Link ]({ successful_run_url })\n")
+
             f.write(f"\n#### Failure Details\n")
             f.write(duckdb.query(f"""
                         SELECT
@@ -42,15 +60,9 @@ def count_consecutive_failures():
                             url
                         FROM '{ input_file }' 
                         WHERE conclusion = 'failure'
-                        LIMIT '{ failures }'
+                        LIMIT '{ last_success }'
                 """).to_df().to_markdown(index=False)
             )
-    else:
-        with open("nightly_failures_{}.md".format(nightly_build), 'a') as f:
-            f.write(f"\n**{ nightly_build }** nightly-build has succeeded.\n")
-    url = duckdb.sql(f"SELECT url FROM '{ input_file }' LIMIT 1").fetchone()[0]
-    with open("nightly_failures_{}.md".format(nightly_build), 'a') as f:
-        f.write(f"\n\nSee the latest run: [ Run Link ]({ url })\n")
 
 def main():
     count_consecutive_failures()
